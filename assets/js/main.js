5bedd80796b738c21143d160b6309c26b831efe1
Ash8808 <asherpt888@gmail.com>
2026-02-09 16:16:28 -0500

    eeeeeeee

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,4 @@
 // Entry JavaScript for the project
 console.log('assets/js/main.js loaded');
 
-document.addEventListener('DOMContentLoaded', () => {
-  const h1 = document.querySelector('h1');
-  if (h1) h1.textContent = h1.textContent.trim();
-});
+
